95f54c7f4546e718fbba2a679a707e6d431766c0
VullnetJ <v.jata24@gmail.com>
2021-08-29 20:10:05 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Greeting from "./Greeting";
+import { PeopleList } from "./PeopleList";
 
+const people = [
+  {
+  name: 'John',
+  age: 40,
+  hairColor: 'blond',
+},
+{
+  name: 'Helen',
+  age: 42,
+  hairColor: 'brown',
+},
+{
+  name: 'Ola',
+  age: 33,
+  hairColor: 'brown',
+},
+
+]
+//as props: person={{surname: "J", age: 33}}
+  //<Greeting />
 function App() {
+   
+  
+  let url = "https://reactjs.org";
+  let msg = 20;
+
+  const displayAlert = () => {
+    alert('Hello');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+   
+       <Greeting  name="Vullnet"  numberOfMessages={msg}/>
+        <PeopleList people={people}/>
+        <button onClick={() => alert('Hello again')}>Click Me!</button>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+        <a className="App-link" href={url} target="_blank" rel="noreferrer">
+          You can click here to learn more:
         </a>
       </header>
     </div>
-  );
+  );  
 }
 
 export default App;
---
+++ src/PeopleList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { PeopleListItem } from './PeopleListItem';
+
+export const PeopleList = ({people}) => (
+        <> 
+          {people.map((person, i) => 
+              <PeopleListItem person={person} key={i} />
+          )}
+        </>
+        
+    );
+
+
